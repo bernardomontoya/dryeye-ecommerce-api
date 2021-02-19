ff10846051bb23e68a6bb61dace073b9756dc35d
Bernardo Montoya <ber.montoya12@gmail.com>
2021-02-18 23:41:18 -0600

    fix: add db options

--- config/database.js
+++ config/database.js
@@ -15,6 +15,14 @@ module.exports = ({ env }) => {
           },
           options: {
             ssl: false,
+            debug: true,
+            pool: {
+              min: 0,
+              max: 10,
+              idleTimeoutMillis: 30000,
+              createTimeoutMillis: 30000,
+              acquireTimeoutMillis: 30000,
+            },
           },
         },
       },
