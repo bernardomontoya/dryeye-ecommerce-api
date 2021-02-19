8998411263eeedc0d1617ef99805a50d24e73008
Bernardo Montoya <ber.montoya12@gmail.com>
2021-02-19 08:21:35 -0600

    fix: revert db options

--- config/database.js
+++ config/database.js
@@ -15,15 +15,6 @@ module.exports = ({ env }) => {
           },
           options: {
             ssl: false,
-            debug: true,
-            pool: {
-              min: 0,
-              max: 10,
-              idleTimeoutMillis: 30000,
-              createTimeoutMillis: 30000,
-              acquireTimeoutMillis: 30000,
-              propagateCreateError: false,
-            },
           },
         },
       },
