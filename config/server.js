2cc63f21bae78c78efd18d02cca56e620796a0c4
Bernardo Montoya <ber.montoya12@gmail.com>
2021-02-18 22:54:25 -0600

    init: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b6d6832765f402f014b346eea92cc1e8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '938fd9bd-3e88-4ae8-bd62-7dc479403a22'
+};
